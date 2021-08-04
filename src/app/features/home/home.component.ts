a8e963bc6541bf1ba092d9c0281a10bc7e8e4dcd
TMargherita <margherita.testi@gmail.com>
2021-08-04 11:58:12 +0200

    home component- leila Version

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,21 +1,16 @@
-<<<<<<< HEAD
 import { WelcomeComponent } from './../welcome/welcome.component';
 import { Welcome } from 'src/app/model/home';
-import { Component, Input, OnInit } from '@angular/core';
 
 
-=======
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { User } from 'src/app/core/models/user';
 import { UserService } from 'src/app/core/services/user.services';
->>>>>>> 89345bd04b38bbf1b6fa65ab557309b2c2f39605
 
 @Component({
   selector: 'app-home',
   template: `
-<<<<<<< HEAD
     <div class="container">
 
         <app-welcome [items]="welcome"></app-welcome>
@@ -25,19 +20,16 @@ import { UserService } from 'src/app/core/services/user.services';
 
     </div>
 
-=======
-<h1>welcome {{user?.name}}</h1>
->>>>>>> 89345bd04b38bbf1b6fa65ab557309b2c2f39605
   `,
   styles: [
   ]
 })
 
 export class HomeComponent implements OnInit {
-user : User | null = null;
-params: string = '';
+  [x: string]: any;
+  user : User | null = null;
+  params: string = '';
 
-<<<<<<< HEAD
 
   welcome : Welcome [] = [
     { label: 'Welcome in your favourite Place',
@@ -55,11 +47,8 @@ params: string = '';
   ]
   constructor() {
 
-=======
-  constructor(private route: ActivatedRoute,
-              private userService: UserService) {
+  constructor(private route: ActivatedRoute, private userService: UserService) {
     console.log(route.snapshot.queryParamMap);
->>>>>>> 89345bd04b38bbf1b6fa65ab557309b2c2f39605
   }
 
   ngOnInit(): void {
@@ -79,4 +68,5 @@ params: string = '';
   }
 
 
+}
 }
